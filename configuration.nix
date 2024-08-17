612cc1bbec70ac51605deb7640e0b41bbd334bc0
Aaron Thapa <notsmart98@gmail.com>
2024-08-17 18:09:35 -0400

    add home manager

--- configuration.nix
+++ configuration.nix
@@ -60,20 +60,20 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+#  hardware.pulseaudio.enable = false;
+#  security.rtkit.enable = true;
+#  services.pipewire = {
+#    enable = true;
+#    alsa.enable = true;
+#    alsa.support32Bit = true;
+#    pulse.enable = true;
+#    # If you want to use JACK applications, uncomment this
+#    #jack.enable = true;
+#
+#    # use the example session manager (no others are packaged yet so this is enabled by default,
+#    # no need to redefine it in your config for now)
+#    #media-session.enable = true;
+#  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
@@ -92,6 +92,13 @@
   # Enable automatic login for the user.
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = "tablet";
+
+  home-manager = {
+    extraSpecialArgs = {inherit inputs; };
+    users = {
+      "user" = import ./home.nix;
+    };
+  };
 
   # Install firefox.
   programs.firefox.enable = true;
