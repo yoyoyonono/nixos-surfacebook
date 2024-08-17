b4452e324a2eacad040c7bf2a965dc847b82569b
Aaron Thapa <notsmart98@gmail.com>
2024-08-17 14:43:24 -0400

    add flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nix-flatpak.url = "github:gmodena/nix-flatpak";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nixos-hardware, nix-flatpak, ... }@inputs:
+    {
+      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+          specialArgs = {inherit inputs;};
+          modules = [ 
+            ./configuration.nix
+            inputs.home-manager.nixosModules.default
+            nix-flatpak.nixosModules.nix-flatpak
+          ];
+        };
+    };
+}
