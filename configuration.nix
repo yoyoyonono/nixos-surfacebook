44ec4f1de55aebc2eb29eec325a770b647f350cc
Aaron Thapa <notsmart98@gmail.com>
2024-08-20 09:18:00 +0545

    timezone

--- configuration.nix
+++ configuration.nix
@@ -25,7 +25,7 @@
   networking.networkmanager.enable = true;
 
   # Set your time zone.
-  time.timeZone = "America/New_York";
+  time.timeZone = "Asia/Kathmandu";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
